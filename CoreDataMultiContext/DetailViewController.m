54dd7431e99cb200960388e42464f342562f4988
Matthew Morey <matthew.morey@gmail.com>
2013-03-18 18:26:13 -0500

    Fixed detail view and added navigation title.

--- CoreDataMultiContext/DetailViewController.m
+++ CoreDataMultiContext/DetailViewController.m
@@ -31,7 +31,7 @@
     // Update the user interface for the detail item.
 
     if (self.detailItem) {
-        self.detailDescriptionLabel.text = [[self.detailItem valueForKey:@"timeStamp"] description];
+        self.detailDescriptionLabel.text = [[self.detailItem valueForKey:@"name"] description];
     }
 }
 
