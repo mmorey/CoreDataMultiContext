e1f845a7d876d10953676470f809dfa284805219
Matthew Morey <matthew.morey@gmail.com>
2013-03-19 08:45:36 -0500

    Enabled NSFetchedResultsController cache

--- CoreDataMultiContext/MasterViewController.m
+++ CoreDataMultiContext/MasterViewController.m
@@ -206,7 +206,7 @@
     
     // Edit the section name key path and cache name if appropriate.
     // nil for section name key path means "no sections".
-    NSFetchedResultsController *aFetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest managedObjectContext:self.managedObjectContext sectionNameKeyPath:nil cacheName:nil];
+    NSFetchedResultsController *aFetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest managedObjectContext:self.managedObjectContext sectionNameKeyPath:nil cacheName:@"default-cache"];
     aFetchedResultsController.delegate = self;
     self.fetchedResultsController = aFetchedResultsController;
     
